ea1f8413bf4faa8f8075153e4b61f8a86ecf2859
root <root@LAPTOP-R3SPKBB2>
2024-11-21 22:35:36 +0800

    finish check7

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -70,7 +70,7 @@ add_custom_target (check3 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --s
 
 add_custom_target (check5 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 120 -R '^net_interface')
 
-add_custom_target (check6 COMMAND ${CMAKE_CTEST_COMMAND} --verbose --stop-on-failure --timeout 120 -R '^net_interface|^router')
+add_custom_target (check6 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 120 -R '^net_interface|^router')
 
 ###
 
